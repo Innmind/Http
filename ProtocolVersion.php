280ef758fa9112d1e65508bb9e60c4ebd3fa0f01
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-24 16:21:05 +0200

    add protocol version implementation

---
+++ ProtocolVersion.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http;
+
+final class ProtocolVersion implements ProtocolVersionInterface
+{
+    private $major;
+    private $minor;
+
+    public function __construct(int $major, int $minor)
+    {
+        $this->major = $major;
+        $this->minor = $minor;
+    }
+
+    public function major(): int
+    {
+        return $this->major;
+    }
+
+    public function minor(): int
+    {
+        return $this->minor;
+    }
+
+    public function __toString(): string
+    {
+        return $this->major.'.'.$this->minor;
+    }
+}
