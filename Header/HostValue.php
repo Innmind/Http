73d7e17ba40f6596bddb00f7c312e17207c1a06c
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-24 13:32:30 +0200

    add host header

---
+++ Header/HostValue.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Header;
+
+use Innmind\Url\Authority\{
+    Host as UrlHost,
+    PortInterface,
+    NullPort
+};
+
+final class HostValue extends HeaderValue
+{
+    private $host;
+    private $port;
+
+    public function __construct(UrlHost $host, PortInterface $port)
+    {
+        $this->host = $host;
+        $this->port = $port;
+
+        parent::__construct($host.(!$port instanceof NullPort ? ':'.$port : ''));
+    }
+
+    public function host(): UrlHost
+    {
+        return $this->host;
+    }
+
+    public function port(): PortInterface
+    {
+        return $this->port;
+    }
+}
