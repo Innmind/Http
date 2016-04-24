203ff777baa812c0bf8f5382bc1df2c16e56d873
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-24 11:41:10 +0200

    add content length header

---
+++ Header/ContentLengthValue.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Header;
+
+use Innmind\Http\Exception\InvalidArgumentException;
+use Innmind\Immutable\StringPrimitive as Str;
+
+final class ContentLengthValue extends HeaderValue
+{
+    public function __construct(int $age)
+    {
+        if ($age < 0) {
+            throw new InvalidArgumentException;
+        }
+
+        parent::__construct((string) $age);
+    }
+}
