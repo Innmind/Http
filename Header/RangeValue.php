73415724b32869580eb2cc6179cc790b3ce13ff7
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-24 13:49:47 +0200

    add range header

---
+++ Header/RangeValue.php
@@ -0,0 +1,54 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Header;
+
+use Innmind\Http\Exception\InvalidArgumentException;
+use Innmind\Immutable\StringPrimitive as Str;
+
+final class RangeValue extends HeaderValue
+{
+    private $unit;
+    private $firstPosition;
+    private $lastPosition;
+
+    public function __construct(
+        string $unit,
+        int $firstPosition,
+        int $lastPosition
+    ) {
+        if (
+            !(new Str($unit))->match('~^\w+$~') ||
+            $firstPosition < 0 ||
+            $lastPosition < 0 ||
+            $firstPosition > $lastPosition
+        ) {
+            throw new InvalidArgumentException;
+        }
+
+        $this->unit = $unit;
+        $this->firstPosition = $firstPosition;
+        $this->lastPosition = $lastPosition;
+        parent::__construct(sprintf(
+            '%s=%s-%s',
+            $unit,
+            $firstPosition,
+            $lastPosition
+        ));
+    }
+
+    public function unit(): string
+    {
+        return $this->unit;
+    }
+
+    public function firstPosition(): int
+    {
+        return $this->firstPosition;
+    }
+
+    public function lastPosition(): int
+    {
+        return $this->lastPosition;
+    }
+}
