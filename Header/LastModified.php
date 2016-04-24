43adc390a5b864e47a930eab675edff9dc8ffcd4
Baptiste Langlade <baptouuuu@gmail.com>
2016-04-24 13:42:41 +0200

    add last modified header

---
+++ Header/LastModified.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Header;
+
+use Innmind\Immutable\Set;
+
+final class LastModified extends Header
+{
+    public function __construct(DateValue $date)
+    {
+        parent::__construct(
+            'Last-Modified',
+            (new Set(HeaderValueInterface::class))
+                ->add($date)
+        );
+    }
+}
