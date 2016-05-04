fd1f515d1b1c54a741392a13358eee1efa48a6a5
Baptiste Langlade <baptouuuu@gmail.com>
2016-05-04 11:32:05 +0200

    add accept encoding header factory

---
+++ Factory/Header/AcceptEncodingFactory.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Factory\Header;
+
+use Innmind\Http\{
+    Factory\HeaderFactoryInterface,
+    Header\HeaderInterface,
+    Header\HeaderValueInterface,
+    Header\AcceptEncodingValue,
+    Header\AcceptEncoding,
+    Header\Quality
+};
+use Innmind\Immutable\{
+    StringPrimitive as Str,
+    Set
+};
+
+final class AcceptEncodingFactory implements HeaderFactoryInterface
+{
+    public function make(Str $name, Str $value): HeaderInterface
+    {
+        $values = new Set(HeaderValueInterface::class);
+
+        foreach ($value->split(',') as $accept) {
+            $matches = $accept->getMatches(
+                '~(?<coding>(\w+|\*))(; ?q=(?<quality>\d+(\.\d+)?))?~'
+            );
+
+            $values = $values->add(
+                new AcceptEncodingValue(
+                    (string) $matches->get('coding'),
+                    new Quality(
+                        $matches->hasKey('quality') ?
+                            (float) (string) $matches->get('quality') : 1
+                    )
+                )
+            );
+        }
+
+        return new AcceptEncoding($values);
+    }
+}
