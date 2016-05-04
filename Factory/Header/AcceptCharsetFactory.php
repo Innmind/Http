4792789f849f77f2f89486ce9505fc2e9c82e848
Baptiste Langlade <baptouuuu@gmail.com>
2016-05-04 11:17:12 +0200

    add accept charset header factory

---
+++ Factory/Header/AcceptCharsetFactory.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Factory\Header;
+
+use Innmind\Http\{
+    Factory\HeaderFactoryInterface,
+    Header\HeaderInterface,
+    Header\HeaderValueInterface,
+    Header\AcceptCharsetValue,
+    Header\AcceptCharset,
+    Header\Quality
+};
+use Innmind\Immutable\{
+    StringPrimitive as Str,
+    Set
+};
+
+final class AcceptCharsetFactory implements HeaderFactoryInterface
+{
+    public function make(Str $name, Str $value): HeaderInterface
+    {
+        $values = new Set(HeaderValueInterface::class);
+
+        foreach ($value->split(',') as $accept) {
+            $matches = $accept->getMatches(
+                '~(?<charset>[a-zA-Z0-9\-_:\(\)]+)(; ?q=(?<quality>\d+(\.\d+)?))?~'
+            );
+
+            $values = $values->add(
+                new AcceptCharsetValue(
+                    (string) $matches->get('charset'),
+                    new Quality(
+                        $matches->hasKey('quality') ?
+                            (float) (string) $matches->get('quality') : 1
+                    )
+                )
+            );
+        }
+
+        return new AcceptCharset($values);
+    }
+}
