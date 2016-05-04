d5c5e49091ea30795c01b26989f96274d3542642
Baptiste Langlade <baptouuuu@gmail.com>
2016-05-04 12:34:44 +0200

    add if unmodified since header factory

---
+++ Tests/Factory/Header/IfUnmodifiedSinceFactoryTest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Tests\Factory\Header;
+
+use Innmind\Http\{
+    Factory\Header\IfUnmodifiedSinceFactory,
+    Factory\HeaderFactoryInterface,
+    Header\IfUnmodifiedSince
+};
+use Innmind\Immutable\StringPrimitive as Str;
+
+class IfUnmodifiedSinceFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMake()
+    {
+        $f = new IfUnmodifiedSinceFactory;
+
+        $this->assertInstanceOf(HeaderFactoryInterface::class, $f);
+
+        $h = $f->make(
+            new Str('If-Unmodified-Since'),
+            new Str('Tue, 15 Nov 1994 08:12:31 GMT')
+        );
+
+        $this->assertInstanceOf(IfUnmodifiedSince::class, $h);
+        $this->assertSame(
+            'If-Unmodified-Since : Tue, 15 Nov 1994 08:12:31 +0000',
+            (string) $h
+        );
+    }
+}
