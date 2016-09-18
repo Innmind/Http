dfd6c181ca3875e73270d9481a8ff1fa5dc34212
Baptiste Langlade <baptouuuu@gmail.com>
2016-09-18 17:29:24 +0200

    add psr7 response translator

---
+++ src/Translator/Response/Psr7Translator.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Translator\Response;
+
+use Innmind\Http\{
+    Factory\Header\DefaultFactory,
+    Message\Response,
+    Message\StatusCode,
+    Message\ReasonPhrase,
+    ProtocolVersion,
+    Headers,
+    Header\HeaderInterface
+};
+use Innmind\Filesystem\Stream\StringStream;
+use Innmind\Immutable\{
+    Map,
+    StringPrimitive as Str
+};
+use Psr\Http\Message\ResponseInterface;
+
+final class Psr7Translator
+{
+    private $headerFactory;
+
+    public function __construct(DefaultFactory $headerFactory)
+    {
+        $this->headerFactory = $headerFactory;
+    }
+
+    public function translate(ResponseInterface $response): Response
+    {
+        list($major, $minor) = explode('.', $response->getProtocolVersion());
+
+        return new Response(
+            new StatusCode($code = $response->getStatusCode()),
+            new ReasonPhrase(ReasonPhrase::defaults()->get($code)),
+            new ProtocolVersion((int) $major, (int) $minor),
+            $this->translateHeaders($response->getHeaders()),
+            new StringStream((string) $response->getBody())
+        );
+    }
+
+    private function translateHeaders(array $rawHeaders): Headers
+    {
+        $headers = new Map('string', HeaderInterface::class);
+
+        foreach ($rawHeaders as $name => $values) {
+            $header = $this->headerFactory->make(
+                new Str($name),
+                new Str(implode(', ', $values))
+            );
+            $headers = $headers->put(
+                $header->name(),
+                $header
+            );
+        }
+
+        return new Headers($headers);
+    }
+}
---
+++ tests/Translator/Response/Psr7TranslatorTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Http\Translator\Response;
+
+use Innmind\Http\{
+    Translator\Response\Psr7Translator,
+    Factory\Header\DefaultFactory,
+    Factory\HeaderFactoryInterface,
+    Message\Response
+};
+use Innmind\Immutable\Map;
+use Psr\Http\Message\{
+    ResponseInterface,
+    StreamInterface
+};
+
+class Psr7TranslatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInterface()
+    {
+        $translator = new Psr7Translator(
+            new DefaultFactory(
+                new Map('string', HeaderFactoryInterface::class)
+            )
+        );
+        $response = $this->createMock(ResponseInterface::class);
+        $response
+            ->expects($this->once())
+            ->method('getStatusCode')
+            ->willReturn(201);
+        $response
+            ->expects($this->once())
+            ->method('getProtocolVersion')
+            ->willReturn('1.1');
+        $response
+            ->expects($this->once())
+            ->method('getHeaders')
+            ->willReturn([
+                'content-type' => ['application/json'],
+            ]);
+        $response
+            ->expects($this->once())
+            ->method('getBody')
+            ->willReturn($stream = $this->createMock(StreamInterface::class));
+        $stream
+            ->expects($this->once())
+            ->method('__toString')
+            ->willReturn('content');
+
+        $response = $translator->translate($response);
+
+        $this->assertInstanceOf(Response::class, $response);
+        $this->assertSame(201, $response->statusCode()->value());
+        $this->assertSame('Created', (string) $response->reasonPhrase());
+        $this->assertSame('1.1', (string) $response->protocolVersion());
+        $headers = $response->headers();
+        $this->assertCount(1, $headers);
+        $this->assertSame(
+            'content-type : application/json',
+            (string) $headers->get('content-type')
+        );
+        $this->assertSame('content', (string) $response->body());
+    }
+}
