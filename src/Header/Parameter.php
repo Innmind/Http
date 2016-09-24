77e3d4287ff7c1adc129369213e55d9137d67b33
Baptiste Langlade <baptouuuu@gmail.com>
2016-09-24 16:31:28 +0200

    quote parameters when needed

--- src/Header/Parameter.php
+++ src/Header/Parameter.php
@@ -3,15 +3,33 @@ declare(strict_types = 1);
 
 namespace Innmind\Http\Header;
 
+use Innmind\Immutable\StringPrimitive as Str;
+
 class Parameter implements ParameterInterface
 {
     private $name;
     private $value;
+    private $string;
 
     public function __construct(string $name, string $value)
     {
+        $value = (new Str($value))->trim();
+
+        if ($value->match('/ /')) {
+            $value = $value
+                ->trim('"')
+                ->append('"')
+                ->prepend('"');
+        }
+
         $this->name = $name;
-        $this->value = $value;
+        $this->value = (string) $value;
+        $this->string = sprintf(
+            '%s%s%s',
+            $this->name,
+            strlen($this->value) > 0 ? '=' : '',
+            strlen($this->value) > 0 ? $this->value : ''
+        );
     }
 
     public function name(): string
@@ -26,11 +44,6 @@ class Parameter implements ParameterInterface
 
     public function __toString(): string
     {
-        return sprintf(
-            '%s%s%s',
-            $this->name,
-            strlen($this->value) > 0 ? '=' : '',
-            strlen($this->value) > 0 ? $this->value : ''
-        );
+        return $this->string;
     }
 }
