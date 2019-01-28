e235707f48968715e5a135c1595572d9d793b648
Baptiste Langlade <baptouuuu@gmail.com>
2019-01-28 19:47:00 +0100

    add request implementation that can be echoed in raw form

---
+++ src/Message/Request/Stringable.php
@@ -0,0 +1,61 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Http\Message\Request;
+
+use Innmind\Http\{
+    Message\Request as RequestInterface,
+    Message\Method,
+    ProtocolVersion,
+    Headers,
+};
+use Innmind\Url\UrlInterface;
+use Innmind\Stream\Readable;
+
+final class Stringable implements RequestInterface
+{
+    private $request;
+
+    public function __construct(RequestInterface $request)
+    {
+        $this->request = $request;
+    }
+
+    public function url(): UrlInterface
+    {
+        return $this->request->url();
+    }
+
+    public function method(): Method
+    {
+        return $this->request->method();
+    }
+
+    public function protocolVersion(): ProtocolVersion
+    {
+        return $this->request->protocolVersion();
+    }
+
+    public function headers(): Headers
+    {
+        return $this->request->headers();
+    }
+
+    public function body(): Readable
+    {
+        return $this->request->body();
+    }
+
+    public function __toString(): string
+    {
+        $headers = \iterator_to_array($this->headers());
+        $headers = \implode("\n", $headers);
+
+        return <<<RAW
+{$this->method()} {$this->url()} HTTP/{$this->protocolVersion()}
+$headers
+
+{$this->body()}
+RAW;
+    }
+}
---
+++ tests/Message/Request/StringableTest.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Http\Message\Request;
+
+use Innmind\Http\{
+    Message\Request\Stringable,
+    Message\Request\Request,
+    Message\Request as RequestInterface,
+    Message\Method\Method,
+    ProtocolVersion\ProtocolVersion,
+    Headers\Headers,
+    Header\ContentType,
+    Header\ContentTypeValue,
+};
+use Innmind\Url\Url;
+use Innmind\Filesystem\Stream\StringStream;
+use PHPUnit\Framework\TestCase;
+
+class StringableTest extends TestCase
+{
+    public function testInterface()
+    {
+        $request = new Request(
+            $url = Url::fromString('http://example.com/foo/bar?query=string'),
+            Method::post(),
+            new ProtocolVersion(2, 0),
+            Headers::of(
+                new ContentType(
+                    new ContentTypeValue('text', 'plain')
+                )
+            ),
+            new StringStream('some body')
+        );
+        $stringable = new Stringable($request);
+
+        $this->assertInstanceOf(RequestInterface::class, $stringable);
+        $this->assertSame($request->url(), $stringable->url());
+        $this->assertSame($request->method(), $stringable->method());
+        $this->assertSame($request->protocolVersion(), $stringable->protocolVersion());
+        $this->assertSame($request->headers(), $stringable->headers());
+        $this->assertSame($request->body(), $stringable->body());
+        $expected = <<<RAW
+POST http://example.com/foo/bar?query=string HTTP/2.0
+Content-Type: text/plain
+
+some body
+RAW;
+        $this->assertSame($expected, (string) $stringable);
+    }
+}
