e8f96a9c62aa3e58995a037e7185f711f43e2cdf
Baptiste Langlade <baptouuuu@gmail.com>
2021-04-03 11:40:39 +0200

    remove unused variable

--- src/ResponseSender.php
+++ src/ResponseSender.php
@@ -72,9 +72,9 @@ final class ResponseSender implements Sender
     {
         /** @psalm-suppress ArgumentTypeCoercion */
         $cookie->values()->foreach(static function(CookieValue $value): void {
-            $parameters = $value->parameters()->reduce(
+            $parameters = $value->parameters()->values()->reduce(
                 [],
-                static function(array $parameters, string $name, Parameter $parameter): array {
+                static function(array $parameters, Parameter $parameter): array {
                     switch ($parameter->name()) {
                         case 'Domain':
                             $parameters['domain'] = $parameter->value();
