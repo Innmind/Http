33cdb993f435adb36138693f65da59ef1e6078c6
Baptiste Langlade <baptouuuu@gmail.com>
2021-04-03 11:27:46 +0200

    fix psalm not understanding headers implement the interface

--- src/Header/AcceptLanguage.php
+++ src/Header/AcceptLanguage.php
@@ -3,10 +3,13 @@ declare(strict_types = 1);
 
 namespace Innmind\Http\Header;
 
+use Innmind\Http\Header as HeaderInterface;
+
 /**
  * @extends Header<AcceptLanguageValue>
+ * @implements HeaderInterface<AcceptLanguageValue>
  */
-final class AcceptLanguage extends Header
+final class AcceptLanguage extends Header implements HeaderInterface
 {
     public function __construct(AcceptLanguageValue ...$values)
     {
