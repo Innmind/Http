b140e85cf0f7814789e485a13af53e5eaa2c5921
Baptiste Langlade <baptiste.langlade@hey.com>
2025-04-19 16:28:44 +0200

    inline the default case inside the factory class

--- src/Factory/Header/Factory.php
+++ src/Factory/Header/Factory.php
@@ -3,7 +3,10 @@ declare(strict_types = 1);
 
 namespace Innmind\Http\Factory\Header;
 
-use Innmind\Http\Header;
+use Innmind\Http\{
+    Header,
+    Header\Value,
+};
 use Innmind\TimeContinuum\Clock;
 use Innmind\Immutable\Str;
 
@@ -13,13 +16,9 @@ use Innmind\Immutable\Str;
  */
 final class Factory
 {
-    private Clock $clock;
-    private HeaderFactory $fallback;
-
-    private function __construct(Clock $clock)
-    {
-        $this->clock = $clock;
-        $this->fallback = new HeaderFactory;
+    private function __construct(
+        private Clock $clock,
+    ) {
     }
 
     public function __invoke(Str $name, Str $value): Header
@@ -31,15 +30,32 @@ final class Factory
                 ->try($this->clock, $value)
                 ->match(
                     static fn($header) => $header,
-                    fn() => ($this->fallback)($name, $value),
+                    static fn() => self::default($name, $value),
                 );
         }
 
-        return ($this->fallback)($name, $value);
+        return self::default($name, $value);
     }
 
     public static function new(Clock $clock): self
     {
         return new self($clock);
+    }
+
+    /**
+     * @psalm-pure
+     */
+    private static function default(Str $name, Str $value): Header
+    {
+        $values = $value
+            ->split(',')
+            ->map(static fn($value) => $value->trim())
+            ->map(static fn($value) => new Value\Value($value->toString()))
+            ->toList();
+
+        return new Header\Header(
+            $name->toString(),
+            ...$values,
+        );
     }
 }
--- tests/Factory/Header/TryFactoryTest.php
+++ tests/Factory/Header/TryFactoryTest.php
@@ -5,6 +5,7 @@ namespace Tests\Innmind\Http\Factory\Header;
 
 use Innmind\Http\{
     Factory\Header\Factory,
+    Header,
     Header\Allow,
     Header\Age,
 };
@@ -30,5 +31,16 @@ class TryFactoryTest extends TestCase
         $factory = Factory::new(Clock::live());
 
         $this->assertInstanceOf(Allow::class, ($factory)($name, $value));
+    }
+
+    public function testUnknownHeader()
+    {
+        $header = Factory::new(Clock::live())(
+            Str::of('X-Foo'),
+            Str::of('bar'),
+        );
+
+        $this->assertInstanceOf(Header::class, $header);
+        $this->assertSame('X-Foo: bar', $header->toString());
     }
 }
